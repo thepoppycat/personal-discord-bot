8aa8abaa463e0f68e66551ecfb4e243118306dfe
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2020-04-21 23:24:41 +0800

    Created request to keep other bot awake

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const client = new Discord.Client()
 const dotenv = require('dotenv'),
       path   = require('path')
 dotenv.config({path: path.join(__dirname, '../.env')})
+const request = require('request');
 
 client.on('ready', () => {
     // Set bot status to: "Watching for a &help in chat"
@@ -10,7 +11,6 @@ client.on('ready', () => {
 	
 })
 
-var xhttp = new XMLHttpRequest();
 
 client.on('message', (receivedMessage) => {
 	if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
@@ -21,10 +21,18 @@ client.on('message', (receivedMessage) => {
 	if (fullCommand == "ea275938-1b07-4633-9db2-52e78bd38e14"){
 		receivedMessage.channel.send("&848b3356-d38b-4ca3-88d8-7e0303337f1b");
 	}
-	xhttp.open("GET", "https://covid-tester.herokuapp.com", true);
-	console.log("bruh");
-	xhttp.send();
+	wakeBot();
 })
+
+function wakePartnerBot(){
+	// Insert url of other bot here
+	request('https://bbcs-discord-bot.herokuapp.com', { json: false }, (err, res, body) => {
+	  if (err) { return console.log(err); }
+	  //console.log(body.url);
+	  //console.log(body.explanation);
+	});
+	console.log("request sent");
+}
 
 
 bot_secret_token = process.env.DISCORD_BOT_TOKEN;
