8c72758d96ccd0fabcbb36a3c89e42fb82ad4560
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2020-04-23 17:24:50 +0800

    Changed timeout to 20 seconds

--- index.js
+++ index.js
@@ -10,24 +10,50 @@ dotenv.config({path: path.join(__dirname, '../.env')})
 const request = require('request');
 
 
+bot_secret_token = process.env.DISCORD_BOT_TOKEN;
+console.log(bot_secret_token);
+var http = require('http');
+http.createServer(function (req, res) {
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.end('Hello World\n');
+}).listen(process.argv[2]);
+console.log('Server running at http://127.0.0.1:' + process.argv[2]);
+client.login(bot_secret_token)
+
+let COUNTER = 20;
+
 client.on('ready', () => {
     // Set bot status to: "Watching for a &help in chat"
-    client.user.setActivity("for a <SECRET> in chat", {type: "WATCHING"})
+    client.user.setActivity("for a <SECRET> in chat", {type: "WATCHING"});
+	let toggler = setInterval(sendMessage, 1000);
 	
 })
 
 
 client.on('message', (receivedMessage) => {
-	if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
-        //testCommand(receivedMessage);
+	let fullCommand = receivedMessage.content;
+	if (receivedMessage.author == client.user) { // Make bot delete the countdowns
+		if (fullCommand != "&848b3356-d38b-4ca3-88d8-7e0303337f1b") receivedMessage.delete();
 		return
     }
-	let fullCommand = receivedMessage.content;
+	return;
 	if (fullCommand == "ea275938-1b07-4633-9db2-52e78bd38e14"){
-		receivedMessage.channel.send("&848b3356-d38b-4ca3-88d8-7e0303337f1b");
+		if (STATE) receivedMessage.channel.send("&848b3356-d38b-4ca3-88d8-7e0303337f1b");
 	}
-	wakePartnerBot();
+	//wakePartnerBot();
 })
+
+function sendMessage(){
+	if (COUNTER>0){
+		client.channels.cache.get('701792754371395655').send(COUNTER);
+		COUNTER-=1;
+	}
+	else{
+		client.channels.cache.get('701792754371395655').send('&848b3356-d38b-4ca3-88d8-7e0303337f1b');
+		wakePartnerBot();
+		COUNTER = 20;
+	}
+}
 
 function wakePartnerBot(){
 	// Insert url of other bot here
@@ -40,13 +66,3 @@ function wakePartnerBot(){
 	console.log("request sent to "+bot_url);
 }
 
-
-bot_secret_token = process.env.DISCORD_BOT_TOKEN;
-console.log(bot_secret_token);
-var http = require('http');
-http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('Hello World\n');
-}).listen(process.argv[2]);
-console.log('Server running at http://127.0.0.1:' + process.argv[2]);
-client.login(bot_secret_token)
