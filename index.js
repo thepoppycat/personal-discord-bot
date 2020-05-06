95129c9545cc35451380e9ef7b1b7f9d92f29bf0
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2020-05-06 14:12:23 +0800

    fixed bugs

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ bot_secret_token = process.env.DISCORD_BOT_TOKEN;
 console.log(bot_secret_token);
 var http = require(`http`);
 http.createServer(function (req, res) {
-   res.writeHead(200, {`Content-Type`: `text/plain`});
+   res.writeHead(200, {'Content-Type': 'text/plain'});
    res.end(`Hello World\n`);
 }).listen(process.argv[2]);
 console.log(`Server running at http://127.0.0.1:` + process.argv[2]);
